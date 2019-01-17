1021f9fe2167441249335f2a194baf30007a5570
Lunga MAKWAKWA <lmakwakw@e4r10p4.wethinkcode.co.za>
2019-01-17 14:04:01 +0200

    location update

--- chat_messages.php
+++ chat_messages.php
@@ -4,6 +4,7 @@ require_once 'core/init.php';
 
 $db = DB::getInstance();
 $user = new User();
+$user_id = $user->data()->user_id;
 
 /* require_once 'core/init.php';
 $db = DB::getInstance();*/
@@ -13,19 +14,23 @@ switch ($_REQUEST['action']) {
             // $con = new PDO("mysql:host=localhost", "root", "123456");
             // $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // $con->query("USE matcha");
-			$sql = ("SELECT `User`,`userID` FROM `users` JOIN `likes` ON `liker_id`=`userID` WHERE `likee_id` = :user AND `liker_stat` = 1 AND `likee_stat` = 1");
-			
+            $sql1 = ("SELECT `username`,`user_id` FROM `users` JOIN `likes` ON `liker_id`=`user_id` WHERE `likee_id` = $user_id AND `liker_stat` = 1 AND `likee_stat` = 1");
+            echo "sql1 = $sql1";
+            
+            
+
             // $stmt->bindParam(':user', $_SESSION['id']);
             // $stmt->execute();
             // $info = $stmt->fetchAll(PDO::FETCH_ASSOC);
             // $con->query("USE matcha");
-            $sql = ("SELECT `User`,`userID` FROM `users` JOIN `likes` ON `likee_id`=`userID` WHERE `liker_id` = :user AND liker_stat = 1 AND `likee_stat` = 1");
+            $sql2 = ("SELECT `username`,`user_id` FROM `users` JOIN `likes` ON `likee_id`=`user_id` WHERE `liker_id` = $user_id AND liker_stat = 1 AND `likee_stat` = 1");
+            echo "sql2 = $sql2";
             // $stmt->bindParam(':user', $_SESSION['id']);
             // $stmt->execute();
             // $ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
             // $fin = array_merge($ret, $info);
             foreach ($fin as $key => $value) {
-                echo '<p onclick="showPart(this);" id='.$value["User"].' data-pid='.$value["userID"].' class="uname">'.$value["User"].'</p><br>';
+                echo '<p onclick="showPart(this);" id='.$value["username"].' data-pid='.$value["user_id"].' class="uname">'.$value["username"].'</p><br>';
             } 
             // $con = null;
         } catch (PDOException $e) {
@@ -35,13 +40,18 @@ switch ($_REQUEST['action']) {
         break;
         case 'sendMessage':
         $newmsg = $_REQUEST['chat'].$_SESSION['username'] .': '. $_REQUEST['message'];
+        $userid = $_REQUEST['user'];
+        echo "userid = $userid";
+        echo "user_id = $user_id";
+    
         $newmsg = explode('<br>', $newmsg);
         $new = json_encode($newmsg);
         var_dump($new); 
         // $con = new PDO("mysql:host=localhost", "root", "123456");
         // $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // $con->query("USE matcha");
-        $sql = ("UPDATE `likes` SET `chat` = :chat WHERE `liker_id`= :id AND `likee_id`= :usid OR `likee_id`= :id AND `liker_id`= :usid");
+        $sql3 = ("UPDATE `likes` SET `chat` = $new WHERE `liker_id`= $user_id AND `likee_id`= $userid OR `likee_id`= $user_id AND `liker_id`= $userid");
+        echo "sql3 = $sql3";
         // $stmt->bindParam(':id', $_SESSION['id']);
         // $stmt->bindParam(':usid', $_REQUEST['user']);
         // $stmt->bindValue(':chat', $new);
@@ -53,7 +63,8 @@ switch ($_REQUEST['action']) {
 		// $con = new PDO("mysql:host=localhost", "root", "123456");
 		// $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		// $con->query("USE matcha");
-		$sql = ("SELECT * FROM `likes` WHERE `liker_id`= :id AND `likee_id`= :usid OR `likee_id`= :id AND `liker_id`= :usid");
+        $sql4 = ("SELECT * FROM `likes` WHERE `liker_id`= $user_id AND `likee_id`= $userid OR `likee_id`= $user_id AND `liker_id`= $userid");
+        echo "sql4 = $sql4";
 		// $stmt->bindParam(':id', $_SESSION['id']);
 		// $stmt->bindParam(':usid', $_REQUEST['user']);
 		// $stmt->execute();
